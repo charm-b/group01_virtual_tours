cd4acb819cf77cf509d0db7e7acf85f4c3e14bf2
Charm Batilo <charm.batilo@gmail.com>
2020-11-04 15:45:16 -0800

    created auth.js and updated login.html

---
+++ scripts/auth.js
@@ -0,0 +1,15 @@
+login = async () => {
+
+    console.log("something");
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    try {
+        await firebase.auth().signInWithEmailAndPassword(email, pass);
+        window.location.href = "index.html";
+    } catch (error) {
+        alert(error);
+    }
+};
+
+var button = document.getElementById("login-but");
+button.addEventListener("click", login);
