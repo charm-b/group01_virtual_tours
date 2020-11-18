001ebdd7c581a64a9e35439998b3a51580f11c5f
Charm Batilo <charm.batilo@gmail.com>
2020-11-18 10:12:10 -0800

    updated photo.js and created photoUpload.html

--- scripts/photo.js
+++ scripts/photo.js
@@ -31,3 +31,59 @@ loadUIPhoto();
 Data.getPhotoDataWithComments().then(data => {
   console.log(JSON.stringify(data));
 });
+
+function uploadMedia(uri, uploadUri) {
+  return new Promise(async (res, rej) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const ref = firebase.storage().ref(uploadUri);
+    const unsubscribe = ref.put(blob).on(
+      'state_changed',
+      state => {},
+      err => {
+        unsubscribe();
+        rej(err);
+      },
+      async () => {
+        unsubscribe();
+        const url = await ref.getDownloadURL();
+        res(url);
+      }
+    );
+  });
+}
+
+// Upload Data
+var uploadMediaAsync = async uri => {
+  const path = `users/${uid()}/${uuidv4()}.${getFileExtension(uri)}`;
+  return uploadMedia(uri, path);
+};
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // uploadMediaAsync("../Wires.png").then((url)=>{
+    //   console.log(url);
+    // });
+  } else {
+      // No user is signed in.
+  }
+});
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function uid() {
+  return (firebase.auth().currentUser || {}).uid;
+}
+
+function getFileExtension(uri) {
+  return uri
+    .split('.')
+    .pop()
+    .split(/#|\?/)[0];
+}
