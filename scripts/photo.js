a8121ff314d981f456162670bbded4c6c63f9830
Izzy Sato <izzy.sato@gmail.com>
2020-11-21 21:27:31 -0800

    photo page is done

--- scripts/photo.js
+++ scripts/photo.js
@@ -1,36 +1,74 @@
 import * as Data from './photo_data.js';
 
+const DOMEvents = {
+  PageLoaded: 'DOMContentLoaded',
+  Click: 'click'
+};
+
+//need this method for the id starts from 0 otherwise error
+const getSafeId = (id) => `id_${id}`;
+
 const commentTemplate = ({
-  uid,
   comment,
   timestamp = ''
-}) => `
-  <p id="${uid}" class="comment">${comment}</p>
-  <p class="timestamp" data-date="${timestamp}">${Data.getDateFromTimeStamp(timestamp)}</p>`;
+}) => `<li>
+  <p class="comment">${comment}</p>
+  <p class="timestamp" data-date="${timestamp}">${Data.getDateFromTimeStamp(timestamp)}</p>
+  </li>`;
 
 const photoTemplate = ({
   imageUrl,
   id
 }) => `
-  <div id="${id}" class="photoSubContainer">
+  <div data-id="${id}" class="photoSubContainer">
     <img alt="view photo" class="viewImage" src="${imageUrl}" />
+    <i class="fas fa-heart" data-id="${id}"></i>
     <div class="comments photoIconSection">
+    </div>   
+    <button type="button" class="btn btn-info loadButton showAllComments" data-id="${id}"></button>
+    ${replyCommentsForm(id)}
     </div>
+  `;
+
+  const replyCommentsForm = (id) =>
+  `
+  <div class="form-group">
+    <textarea class="form-control replyComment" rows="3" placeholder="add comments"  data-id="${id}"></textarea>
+    <button type="button" class="btn btn-info replyButton" data-id="${id}">Reply</button>
   </div>
   `;
 
+  const loadPhotoLikes = async (userID) => {
+    const {likes =[]} = await Data.loadLikes(userID);
+    likes.forEach(l => {
+      const selector = `.fa-heart[data-id="${l}"]`;
+      console.log(`setting like for ${selector}`);
+      const heart = document.querySelector(selector);
+      if(heart) heart.classList.add('changeColor');
+    });
+  };
+
+// display in HTML
 const loadCommentUI = (photoID) => {
+  //sort
+  //add ul
   Data.loadCommentData(photoID)
     .then(data => {
-      document.querySelector(`#${photoID} .comments`).innerHTML = data.map(doc => commentTemplate(doc))
+      const html = data
+        .sort( (a, b) => a.timestamp - b.timestamp)
+        .map(doc => commentTemplate(doc))
         .join('');
-    })
-}
+        document.querySelector(`div.photoSubContainer[data-id="${photoID}"] .comments`)
+        .innerHTML = `<ul class="commentUl" data-id="${photoID}">${html}</ul>`;
+    });
+};
 
+//display photo to HTML
 const loadUIPhoto = (dataMethod = Data.getPhotoData) => {
   dataMethod()
     .then(data => {
-      document.querySelector('#photoFromDatabase').innerHTML = data.map(doc => photoTemplate(doc)).join('');
+      document.querySelector('#photoFromDatabase').innerHTML = data.map(doc => photoTemplate(doc))
+      .join('');
       try {
         data.forEach(({
           id
@@ -38,13 +76,11 @@ const loadUIPhoto = (dataMethod = Data.getPhotoData) => {
       } catch (err) {
         console.log(err);
       }
+      loadPhotoLikes(uid());
     });
 };
 
-Data.getPhotoDataWithComments().then(data => {
-  console.log(JSON.stringify(data));
-});
-
+// storing photoes to the firestore
 function uploadMedia(uri, uploadUri) {
   return new Promise(async (res, rej) => {
     const response = await fetch(uri);
@@ -69,7 +105,7 @@ function uploadMedia(uri, uploadUri) {
 
 // Upload Data
 var uploadMediaAsync = async uri => {
-  const path = `users/${uid()}/${uuidv4()}.${getFileExtension(uri)}`;
+  const path = `users/${uid()}/${Data.uuidv4()}.${getFileExtension(uri)}`;
   return uploadMedia(uri, path);
 };
 
@@ -83,14 +119,6 @@ firebase.auth().onAuthStateChanged(function (user) {
   }
 });
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16 | 0,
-      v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 function uid() {
   return (firebase.auth().currentUser || {}).uid;
 }
@@ -102,10 +130,8 @@ function getFileExtension(uri) {
     .split(/#|\?/)[0];
 }
 
-
-const initPhotoUpload = () => {
-  document.querySelector('#photoSubmitButton').addEventListener('click', async () => {
-    console.log('clicked');
+// adding to firebase, uploading file, and adding comments
+const photoUpload = async () => {
     const fileInput = document.querySelector('#file-input');
     if (fileInput.value === '') return;
     const uri = URL.createObjectURL(fileInput.files[0]); 
@@ -113,19 +139,67 @@ const initPhotoUpload = () => {
       const imageUrl = await uploadMediaAsync(uri);
       const comment = document.querySelector('#photoComment').value;
       const timestamp = Date.now();
+      const container = document.querySelector('.container');
       Data.addNewPhotoWithComments({
         uid: uid(),
         imageUrl,
         comment,
         timestamp
+      }).then(() => {
+        container.style.display = 'none';
+        Data.sucessMessage();
+      }).catch((err) => {
+        container.style.display = 'none';
+        Data.failMessage(err);
       });
     }
-  });
 };
 
-(() => {
+// add comments to existing photo
+const addComment = ({ dataset : {id} }) => {
+  const comment = document.querySelector(`textarea.replyComment[data-id="${id}"]`).value;
+  if(comment !== ''){
+    Data.addComment({
+      id,
+      comment,
+      timestamp: Date.now() 
+    });
+  }
+  loadUIPhoto();
+};
+
+const toggleCommentShow = ({dataset: { id }}) => {
+  document.querySelector(`div.photoSubContainer[data-id="${id}"]`).classList.toggle('showAll');
+};
+
+// like button
+const likePhoto = ({dataset: {id}}) => {
+  const heart = document.querySelector(`.fa-heart[data-id="${id}"]`);
+  heart.classList.toggle('changeColor');
+  const likeData = {
+    photoID: id,
+    userID: uid()
+  };
+  if(heart.classList.contains('changeColor')){
+    Data.likePhoto(likeData);
+  }else{
+    Data.disLikePhoto(likeData);
+  };
+};
+
+const processClick = (target) =>{
+  if(target.matches('.replyButton')) addComment(target);
+  if(target.matches('#photoSubmitButton')) photoUpload();
+  if(target.matches('.showAllComments')) toggleCommentShow(target);
+  if(target.matches('.fa-heart')) likePhoto(target);
+};
+
+// DOM ready call the functions
+document.addEventListener(DOMEvents.PageLoaded, () => {
   const photoContainer = document.querySelector('#photoFromDatabase');
-  const photoUpload = document.querySelector('#file-input');
+  const photoUploadInput = document.querySelector('#file-input');
   if (photoContainer) loadUIPhoto();
-  if (photoUpload) initPhotoUpload();
-})();
+  if (photoUploadInput) photoUpload();
+  document.addEventListener('click', ({target}) => processClick(target))
+});
+
